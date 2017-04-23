43f47621a25baf94b1320e7c8c2bf81fc5d98352
PopescuAndrei <andrei.popescu93@gmail.com>
2017-04-24 00:49:30 +0300

    fixed db config

---
+++ src/main/resources/sql/chatbot_init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE table_name(
+column1 DATE_TYPE1 PRIMARY KEY,
+column2 DATE_TYPE2,
+column3 DATE_TYPE3,
+column4 DATE_TYPE4
+)
+
+CREATE TABLE Candidate (
+	Id serial primary key not null,
+	FirstName VARCHAR(40) not null,
+	LastName VARCHAR(40) not null,
+	Age INT not null
+)
