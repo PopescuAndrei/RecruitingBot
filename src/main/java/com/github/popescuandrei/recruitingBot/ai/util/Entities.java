ab1c4a30ce1fc8f5e8ad2f09f41c239166683464
Andrei Ioan Popescu <andrei.popescu93@gmail.com>
2017-10-02 17:25:16 +0300

    refactoring

---
+++ src/main/java/com/github/popescuandrei/recruitingBot/ai/util/Entities.java
@@ -0,0 +1,21 @@
+package com.github.popescuandrei.recruitingBot.ai.util;
+
+public final class Entities {
+
+	// common
+	public static final String GENDER = "gender";
+	public static final String PERIOD = "period";
+	public static final String LEVEL = "level";
+	
+	// education
+	public static final String INSTITUTION = "university";
+	public static final String MAJOR = "major";
+	
+	// experience
+	public static final String COMPANY = "company";
+	public static final String POSITION = "position";
+	
+	// skills
+	public static final String SKILL = "skill";
+	
+}
---
+++ src/main/java/com/github/popescuandrei/recruitingBot/ai/util/Intents.java
@@ -0,0 +1,13 @@
+package com.github.popescuandrei.recruitingBot.ai.util;
+
+public final class Intents {
+
+	public static final String ADD_EDUCATION = "add_education";
+	public static final String ADD_EMAIL = "add_email";
+	public static final String ADD_EXPERIENCE = "add_experience";
+	public static final String ADD_SKILLS = "add_skills";
+	public static final String ADD_INTEREST = "add_interest";
+	
+	
+	public static final String SEARCH_POSITIONS = "search_positions";
+}
