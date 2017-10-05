956dde6f149f69bcf2bab5867c032a2a544dd1f7
Andrei Ioan Popescu <andrei.popescu93@gmail.com>
2017-10-05 11:08:14 +0300

    more logs

--- src/main/java/com/github/popescuandrei/recruitingBot/util/AiDecisionService.java
+++ src/main/java/com/github/popescuandrei/recruitingBot/util/AiDecisionService.java
@@ -1,5 +1,16 @@
 package com.github.popescuandrei.recruitingBot.util;
 
+import static com.github.popescuandrei.recruitingBot.ai.util.Actions.ADD_EDUCATION;
+import static com.github.popescuandrei.recruitingBot.ai.util.Actions.ADD_EMAIL;
+import static com.github.popescuandrei.recruitingBot.ai.util.Actions.ADD_EXPERIENCE;
+import static com.github.popescuandrei.recruitingBot.ai.util.Actions.ADD_GENDER;
+import static com.github.popescuandrei.recruitingBot.ai.util.Actions.ADD_INTEREST;
+import static com.github.popescuandrei.recruitingBot.ai.util.Actions.ADD_LANGUAGE;
+import static com.github.popescuandrei.recruitingBot.ai.util.Actions.ADD_SKILLS;
+import static com.github.popescuandrei.recruitingBot.ai.util.Actions.APPLY_POSITION;
+import static com.github.popescuandrei.recruitingBot.ai.util.Actions.GREETINGS;
+import static com.github.popescuandrei.recruitingBot.ai.util.Actions.SEARCH_POSITIONS;
+
 import java.util.HashMap;
 
 import org.slf4j.Logger;
@@ -7,6 +18,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import com.github.popescuandrei.recruitingBot.ai.util.Actions;
 import com.google.gson.JsonElement;
 
 import ai.api.model.AIResponse;
@@ -20,10 +32,33 @@ public class AiDecisionService {
 	public String parseAiResponse(AIResponse aiResponse) {
 		String response = null;
 		
-		if(aiResponse.getResult().getAction().equals("add_experience")) {
-			HashMap<String, JsonElement> parameters = aiResponse.getResult().getParameters();
-			parameters.forEach((k,v) -> LOGGER.debug("PARAM:  " + k + ",VALUE:  " + v.toString()));
+		String debugLog = null;
+		if(aiResponse.getResult().getAction().equals(ADD_EDUCATION)) {
+			debugLog = "ADD_EDUCATION";
+		} else if (aiResponse.getResult().getAction().equals(ADD_EMAIL)) {
+			debugLog = "ADD_email";
+		} else if (aiResponse.getResult().getAction().equals(ADD_EXPERIENCE)) {
+			debugLog = "ADD_experience";
+		} else if (aiResponse.getResult().getAction().equals(ADD_GENDER)) {
+			debugLog = "ADD_Gender";
+		} else if (aiResponse.getResult().getAction().equals(ADD_INTEREST)) {
+			debugLog = "ADD_interest";
+		} else if (aiResponse.getResult().getAction().equals(ADD_LANGUAGE)) {
+			debugLog = "ADD_language";
+		} else if (aiResponse.getResult().getAction().equals(ADD_SKILLS)) {
+			debugLog = "add skills";
+		} else if (aiResponse.getResult().getAction().equals(APPLY_POSITION)) {
+			debugLog = "apply pos";
+		} else if (aiResponse.getResult().getAction().equals(GREETINGS)) {
+			debugLog = "greet";
+		} else if (aiResponse.getResult().getAction().equals(SEARCH_POSITIONS)) {
+			debugLog = "saerch pos";
+		} else {
+			debugLog = "ANYTHING";
 		}
+		LOGGER.debug("ACTIOOOOOOOOOOOOOON : " + debugLog);
+		HashMap<String, JsonElement> parameters = aiResponse.getResult().getParameters();
+		parameters.forEach((k,v) -> LOGGER.debug("PARAM:  " + k + ",VALUE:  " + v.toString()));
 		
 		return response;
 	}
